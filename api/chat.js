f8ec53d6eab235f7c7ba9accfb872e75ddd4c18a
Sacha999999999 <info@alpina-conseil.ch>
2026-02-14 01:19:21 +0100

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
     }
 
     const response = await fetch(
-      "https://api-inference.huggingface.co/models/gpt2",
+      "https://api-inference.huggingface.co/models/distilgpt2",
       {
         method: "POST",
         headers: {
